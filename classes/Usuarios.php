b8987fc39aa19535b7f1108f1db018d24b06c3cf
Dinicley Sousa Alves <dinicleyalvess@gmail.com>
2017-03-07 21:31:32 -0300

    Add files via upload

--- classes/Usuarios.php
+++ classes/Usuarios.php
@@ -7,6 +7,7 @@ class Usuarios extends Crud{
 	protected $table = 'usuarios';
 	private $nome;
 	private $email;
+	private $telefone;
 
 	public function setNome($nome){
 		$this->nome = $nome;
@@ -19,23 +20,31 @@ class Usuarios extends Crud{
 	public function setEmail($email){
 		$this->email = $email;
 	}
+	
+	public function setTelefone($telefone){
+		$this->telefone = $telefone;
+	}
+
+	
 
 	public function insert(){
 
-		$sql  = "INSERT INTO $this->table (nome, email) VALUES (:nome, :email)";
+		$sql  = "INSERT INTO $this->table (nome, email, telefone) VALUES (:nome, :email, :telefone)";
 		$stmt = DB::prepare($sql);
 		$stmt->bindParam(':nome', $this->nome);
 		$stmt->bindParam(':email', $this->email);
+		$stmt->bindParam(':telefone', $this->telefone);
 		return $stmt->execute(); 
 
 	}
 
 	public function update($id){
 
-		$sql  = "UPDATE $this->table SET nome = :nome, email = :email WHERE id = :id";
+		$sql  = "UPDATE $this->table SET nome = :nome, email = :email, telefone = :telefone WHERE id = :id";
 		$stmt = DB::prepare($sql);
 		$stmt->bindParam(':nome', $this->nome);
 		$stmt->bindParam(':email', $this->email);
+		$stmt->bindParam(':telefone', $this->telefone);
 		$stmt->bindParam(':id', $id);
 		return $stmt->execute();
 
--- classes/config.php
+++ classes/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'php-crud');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root123');
 
